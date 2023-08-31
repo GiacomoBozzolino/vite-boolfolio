5c3a904abf20fb23425c1aaa88600bd1df8e1361
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-08-31 13:42:31 +0200

    [FEAT] creazione file NotFound

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name:'NotFound'
+    
+}
+</script>
+
+<template lang="">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>404 Pagina non trovata</h1>
+
+            </div>
+
+        </div>
+        
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
