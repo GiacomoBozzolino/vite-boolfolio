19055fb510b6bed4b879e018f96b06c087406a81
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-09-01 15:14:33 +0200

    [FEAT] lavoro metodo sendForm

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,74 @@
+<script>
+
+import { store } from '../store';
+
+import axios from 'axios';
+    
+    export default {
+        name:'ContactForm',
+
+        data() {
+            return {
+            store,
+            loading: false,
+            name: '',
+            email: '',
+            content: '',
+            errors: {},
+            }
+        },
+
+        methods: {
+            sendForm(){
+                this.loading=true;
+
+                const data = {
+                    name: this.name,
+                    email: this.email,
+                    content: this.message,
+                   
+                };
+                // SVUOTO I MESSAGGI DI ERRORE  
+                this.errors ={},
+
+                // EFFETTUO LA CHIAMATA AXIOS IN POST
+                axios.post(`${store.baseUrl}/api/contacts`, data),then((response)=>{
+                    this.success = response.data.success;
+                    if(!this.success){
+                        this.errors = response.data.errors;
+                    }
+                    else{
+                        // RIPULIAMO I DATI IN INPUT
+                            this.name='';
+                            this.email='';
+                            this.message='';
+                    }
+                    this.loading=false
+                })
+
+
+
+
+
+            }
+
+        },
+        
+    }
+</script>
+
+
+
+
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+
+
+
+
+<style lang="">
+    
+</style>
